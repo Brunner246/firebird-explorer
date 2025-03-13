2edbdf1816aeac86eef8404725cca94e164f4f27
Michael Brunner <brunner@cadwork.swiss>
2025-03-13 14:42:14 +0100

    include functions for printing table names and attributes

--- src/main.py
+++ src/main.py
@@ -1,36 +1,44 @@
 import fdb
 
-con = fdb.connect(
-    host='cw-firebird-db', 
-    database='/firebird/data/CONTENT_3D.FDB',
-    user='sysdba',
-    password='masterkey'
-)
+def print_table_names():
+    con = fdb.connect(
+        host='cw-firebird-db',
+        database='/firebird/data/CONTENT_3D.FDB',
+        user='sysdba',
+        password='masterkey'
+    )
 
-cur = con.cursor()
+    cur = con.cursor()
 
-cur.execute("SELECT RDB$RELATION_NAME FROM RDB$RELATIONS WHERE RDB$RELATION_TYPE = 0 AND RDB$SYSTEM_FLAG = 0")
+    cur.execute("SELECT RDB$RELATION_NAME FROM RDB$RELATIONS WHERE RDB$RELATION_TYPE = 0 AND RDB$SYSTEM_FLAG = 0")
 
-tables = cur.fetchall()
-for table in tables:
-    print(table[0].strip())
+    tables = cur.fetchall()
+    for table in tables:
+        print(table[0].strip())
 
-# Schließe die Verbindung
-con.close()
+    con.close()
 
+def print_cw_attributes():
+    con = fdb.connect(
+        host='cw-firebird-db',
+        database='/firebird/data/CONTENT_3D.FDB',
+        user='sysdba',
+        password='masterkey'
+    )
 
+    cur = con.cursor()
 
-# import fdb
-#
-# con = fdb.connect(
-#     dsn='localhost/3050:/firebird/data/test.fdb',
-#     user='SYSDBA',
-#     password='dein_passwort'
-# )
-#
-# cur = con.cursor()
-# cur.execute("SELECT * FROM my_table")
-# for row in cur.fetchall():
-#     print(row)
-#
-# con.close()
+    cur.execute("SELECT * FROM CW_EXTERNAL_PROJECT_DATA")
+
+    attributes = cur.fetchall()
+    if len(attributes) == 0:
+        print("No external project data found")
+        return
+    for attribute in attributes:
+        print(attribute)
+
+    con.close()
+
+if __name__ == '__main__':
+    print_table_names()
+    print_cw_attributes()
